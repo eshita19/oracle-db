fe8da7aff0a9f746b1f5db81d24e10f8be3ed0ce
eshita19 <33754197+eshita19@users.noreply.github.com>
2020-02-08 10:00:38 +0530

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -10,18 +10,17 @@ CREATE TABLE EMP_TEST
 -- ALL_TABLES: USER_TABLES + tables you have been given access to.
 
 
-----COLUMN LEVEL CONSTRAINTS: UNIQUE, NULL, CHECK, PRIMARY, FOREIGN
+----CONSTRAINTS: UNIQUE, NULL, CHECK, PRIMARY, FOREIGN
 -- CHECK AND FOREIGN KEY CONSTRAINT CAN INSERT NULL VLAUES
+-- COLUMN LEVEL CONSTRAINTS
  CREATE TABLE STUD
  ( STUD_ID VARCHAR(20) CONSTRAINT STUD_PK PRIMARY KEY,
    NUMBER MARKS CONSTRAINT STUD_MARKS_CHECK CHECK(MARKS >20),
    NAME VARCHAR(20) NOT NULL,
    ROLL_NUM VARCHAR(20) UNIQUE
-   DEPT_ID VARCHAR(30) CONSTRAINT STUD_FK REFERENCES DEPARTMENT(DEPT_ID));
-                                                                
--- Check constraints
-SELECT * FROM USER_CONSTRAINTS WHERE TABLE_NAME="STUD"; 
-                                                                
+   DEPT_ID VARCHAR(30) CONSTRAINT STUD_FK REFERENCES DEPARTMENT(DEPT_ID));                                                                
+
+ -- TABLE LEVEL CONSTRAINTS                                                               
  CREATE TABLE STUD
  ( STUD_ID VARCHAR(20),
    NUMBER MARKS,
@@ -32,7 +31,9 @@ SELECT * FROM USER_CONSTRAINTS WHERE TABLE_NAME="STUD";
    CONSTRAINT STUD_MARKS_CHECK CHECK(MARKS >20),
    CONSTRAINT STUD_UNIQUE UNIQUE(ROLL_NUM),
    CONSTRAINT STUD_FK (DEPT_ID) REFERENCES DEPARTMENT(DEPT_ID)
- );                                                                
+ ); 
+ -- Check constraints
+SELECT * FROM USER_CONSTRAINTS WHERE TABLE_NAME="STUD";                                                                
                                                                 
                                                                 
 
