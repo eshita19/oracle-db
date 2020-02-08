efd73b214c39d20bb7a4eff4a550aa612d5e353e
eshita19 <33754197+eshita19@users.noreply.github.com>
2020-02-08 09:59:35 +0530

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -10,9 +10,8 @@ CREATE TABLE EMP_TEST
 -- ALL_TABLES: USER_TABLES + tables you have been given access to.
 
 
----- CONSTRAINTS: UNIQUE, NULL, CHECK, PRIMARY, FOREIGN
+----COLUMN LEVEL CONSTRAINTS: UNIQUE, NULL, CHECK, PRIMARY, FOREIGN
 -- CHECK AND FOREIGN KEY CONSTRAINT CAN INSERT NULL VLAUES
-
  CREATE TABLE STUD
  ( STUD_ID VARCHAR(20) CONSTRAINT STUD_PK PRIMARY KEY,
    NUMBER MARKS CONSTRAINT STUD_MARKS_CHECK CHECK(MARKS >20),
@@ -21,6 +20,20 @@ CREATE TABLE EMP_TEST
    DEPT_ID VARCHAR(30) CONSTRAINT STUD_FK REFERENCES DEPARTMENT(DEPT_ID));
                                                                 
 -- Check constraints
-SELECT * FROM USER_CONSTRAINTS WHERE TABLE_NAME="STUD";                                                                
+SELECT * FROM USER_CONSTRAINTS WHERE TABLE_NAME="STUD"; 
                                                                 
+ CREATE TABLE STUD
+ ( STUD_ID VARCHAR(20),
+   NUMBER MARKS,
+   NAME VARCHAR(20) NOT NULL, -- NOT NULL HAS TO BE INLINE
+   ROLL_NUM VARCHAR(20) 
+   DEPT_ID VARCHAR(30),
+   CONSTRAINT STUD_PK PRIMARY KEY(STUD_ID, NAME), -- CANNOT ADD COMPOSITE KEY IN METHOD1 
+   CONSTRAINT STUD_MARKS_CHECK CHECK(MARKS >20),
+   CONSTRAINT STUD_UNIQUE UNIQUE(ROLL_NUM),
+   CONSTRAINT STUD_FK (DEPT_ID) REFERENCES DEPARTMENT(DEPT_ID)
+ );                                                                
+                                                                
+                                                                
+
                                                                 
