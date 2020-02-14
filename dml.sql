9d9f11b17da6920baa5bc10dc9d0ab3bbc1aa36a
eshita19 <33754197+eshita19@users.noreply.github.com>
2020-02-14 10:20:14 +0530

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -106,6 +106,8 @@ EXCEPTION
     exception_handling statements
 END;
 */
+
+--ROW LEVEL TRIGGERS : EXECUTES ONCE FOR EACH ROW IN A TRANSACTION
 CREATE OR REPLACE TRIGGER UPDATE_AUDIT
 AFTER INSERT ON STUDENT 
 FOR EACH ROW 
@@ -132,6 +134,19 @@ BEGIN
    VALUES('CUSTOMERS', l_transaction, USER, SYSDATE);
 END;
 
-
+-- STATEMENT LEVEL TRIGGERS - EXECUTES ONCE FOR A TRANSACTION
+CREATE OR REPLACE TRIGGER customers_credit_trg
+    BEFORE UPDATE OF credit_limit  
+    ON customers
+DECLARE
+    l_day_of_month NUMBER;
+BEGIN
+    -- determine the transaction type
+    l_day_of_month := EXTRACT(DAY FROM sysdate);
+ 
+    IF l_day_of_month BETWEEN 28 AND 31 THEN
+        raise_application_error(-20100,'Cannot update customer credit from 28th to 31st');
+    END IF;
+END;
 
 
