6f4dd88b64001c219ac1c8d4d48f1ad77c693e78
eshita19 <33754197+eshita19@users.noreply.github.com>
2020-02-14 10:14:14 +0530

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -106,6 +106,32 @@ EXCEPTION
     exception_handling statements
 END;
 */
+CREATE OR REPLACE TRIGGER UPDATE_AUDIT
+AFTER INSERT ON STUDENT 
+FOR EACH ROW 
+BEGIN
+INSERT INTO STUD_AUDIT(NAME) VALUES(NEW.NAME);
+END;
+
+
+CREATE OR REPLACE TRIGGER customers_audit_trg
+    AFTER 
+    UPDATE OR DELETE 
+    ON customers
+    FOR EACH ROW    
+DECLARE
+   l_transaction VARCHAR2(10);
+BEGIN
+   -- determine the transaction type
+   l_transaction := CASE  
+         WHEN UPDATING THEN 'UPDATE'
+         WHEN DELETING THEN 'DELETE'
+   END;
+ 
+   -- insert a row into the audit table   
+   INSERT INTO audits (table_name, transaction_name, by_user, transaction_date)
+   VALUES('CUSTOMERS', l_transaction, USER, SYSDATE);
+END;
 
 
 
