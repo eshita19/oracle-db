646c481b2f5adca59ad237f8a5f42a6153058d1f
Eshita Mathur <33754197+eshita19@users.noreply.github.com>
2020-02-19 11:21:26 +0530

    Update pl.sql

--- pl.sql
+++ pl.sql
@@ -1,3 +1,10 @@
+DECLARE
+    l_message varchar(20) := 'Eshita';
+    l_number number;
 BEGIN 
-    DBMS_OUTPUT.put_line('Hello world!');
+    l_number := 1/0;
+    DBMS_OUTPUT.put_line(l_message);
+    EXCEPTION
+        WHEN ZERO_DIVIDE THEN
+            DBMS_OUTPUT.put_line(SQLERRM);
 END;
